c7875c6352b7b4f83c38ef15afb42360ece45f26
timothyrusso <russotimothy@live.it>
2022-09-29 08:29:25 +0200

    feat: new card component and css optimization

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,26 +1,19 @@
 import { Component } from 'react';
+import Card from '../card/card.component';
 import './card-list.styles.css';
 
 class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
 
-    render() {
-        const { monsters } = this.props;
-
-        return (
-            <div>
-                {monsters.map(monster => {
-                    const { name, email, id } = monster;
-                    return (
-                        <div className='card-container' key={id}>
-                            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
-                            <h2>{name}</h2>
-                            <p>{email}</p>
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
+    return (
+      <div className='card-list'>
+        {monsters.map((monster) => {
+          return <Card monster={monster} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default CardList;
