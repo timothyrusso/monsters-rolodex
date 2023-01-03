575065b66db3f95dcdd70587da6485a84bd2730a
timothyrusso <russotimothy@live.it>
2023-01-03 23:10:20 +0100

    feat: migrate to typescript search box component

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,6 +1,17 @@
 import './search-box.styles.css';
+import { ChangeEvent } from 'react';
 
-const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+interface ISearchBoxProps {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: (a: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+}: ISearchBoxProps) => (
   <input
     className={`search-box ${className}`}
     type="search"
